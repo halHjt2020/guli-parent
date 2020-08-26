6fa5195e72b970b93595189856a4271326b47628
黄江泰 <jtH20182019@163.com>
2020-08-26 09:01:51 +0800

    首次代码提交

---
+++ common/service-base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.atguigu.guli.service.base.handler;
+
+
+import com.atguigu.guli.service.base.result.R;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error();
+
+    }
+}
